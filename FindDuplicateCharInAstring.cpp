f4e1debe0c8f1ec49c598d2e15f47922a759bee4
Apkharsh <Apkharsh@gmail.com>
2022-04-12 11:43:47 +0530

    find duplicate character in a string

---
+++ FindDuplicateCharInAstring.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    /*Write your code here*/
+    unordered_map<char, int> mp;
+    string s;
+    cin >> s;
+    for(int i = 0; i < s.length(); i++){
+        mp[s[i]]++;
+    }
+
+    for(auto it = mp.begin(); it != mp.end(); it++){
+        if(it->second > 1){
+            cout << it->first << endl;
+        }
+    }
+    return 0;
+}
